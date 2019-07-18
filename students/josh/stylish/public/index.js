da9fc7d3879aba1fc8e7ff3fdd2e3d6763a710bd
Josh Yang <spadeg39475@gmail.com>
2019-07-18 17:51:14 +0800

    connect to Product Details API and render

--- students/josh/stylish/public/index.js
+++ students/josh/stylish/public/index.js
@@ -53,166 +53,6 @@ function callAPI(callback){
 }
 
 
-
-//----paging scroll -----
-function scroll(){
-    let container = document.querySelector('.category');
-    let distance  = container.getBoundingClientRect().bottom - window.innerHeight;
-    
-    if( distance < triggerDistance){
-
-        callAPI(createProduct);
-    }
-}
-
-window.addEventListener('scroll', scroll);
-
-
-//-----change menu 
-function changeProductType(productType){
-        let e = event.target;
-       
-        //remove old product
-        removeProduct();
-
-        // active menu add color
-        let menuAnchar = document.querySelectorAll('.nav-left .menu a')
-        menuAnchar.forEach(a => a.classList.remove('isActive'));
-        e.classList.add('isActive');
-
-        //call api\
-        stylish.api = "products";
-        stylish.productType = productType;
-        src= "https://api.appworks-school.tw/api/1.0/" + stylish.api + "/" + stylish.productType;
-        callAPI(createProduct);       
-}
-
-
-//移除category
-function removeProduct(){
-    let category = document.querySelector('.category');
-    while(category.firstChild){
-        category.removeChild(category.firstChild);
-    }
-}
-
-
-//生成category
-function createProduct(res){
-    let  categoryBlock = document.querySelector('.category');
-    let  productBlock, newTag, productImg, colorDiv, colorBlock, productName, productPrice;
-
-
-    res.data.forEach( (item ,index)=> {
-
-        // product block
-        productBlock = document.createElement('div');
-        productBlock.classList.add("category-block");
-        
-
-        // new tag
-        if(index === 0){
-            newTag = document.createElement('div');
-            newTag.classList.add("new");
-            newTag.textContent = "新品";
-            productBlock.appendChild(newTag);
-        }
-
-        //product Img
-        productImg = document.createElement('img');
-        productImg.classList.add("sample");
-        productImg.src = item.main_image;
-
-        // product name
-        productName = document.createElement('p');
-        productName.classList.add("sample-name");
-        productName.textContent = item.title;
-
-
-        // color
-        colorDiv = document.createElement('div');
-        item.colors.forEach( color => {
-            colorBlock = document.createElement('div');
-            colorBlock.classList.add("color");
-            colorBlock.style.backgroundColor = "#" + color.code;
-            if(color.code.toUpperCase() === 'FFFFFF'){
-                colorBlock.classList.add("color-border");
-            }
-            colorDiv.appendChild(colorBlock);
-        } )
-
-
-        // price
-        productPrice = document.createElement('p');
-        productPrice.classList.add("sample-price");
-        productPrice.textContent ='TWD.'+ item.price.toString();
-        
-        //append child
-        productBlock.appendChild(productImg);
-        productBlock.appendChild(colorDiv);
-        productBlock.appendChild(productName);
-        productBlock.appendChild(productPrice);
-
-        categoryBlock.appendChild(productBlock);
-    })
-}
-
-
-//------------search ----------------
-function search(value){
-    
-    stylish.productType = "search";
-    src= "https://api.appworks-school.tw/api/1.0/" + stylish.api + "/" + stylish.productType + "?keyword=" +value;
- 
-    
-    removeProduct();
-    callAPI(createProduct);
-
-    let input = document.querySelector('#search');
-    input.value ="";
-}
-
-
-function err_page(){
-
-    removeProduct()
-
-    let  category = document.querySelector('.category');
-    let  productBlock = document.createElement('div');;
-    productBlock.textContent = "請重新搜尋";
-    category.appendChild(productBlock);
-}
-
-
-// ----mobile search ----------//
-let mobile_search = document.querySelector('.mobile-search-img');
-let mobile_search_input = document.querySelector('.mobile-search-input');
-
-mobile_search.addEventListener('click', () =>{
-    mobile_search.classList.add('hidden');
-    mobile_search_input.classList.remove('hidden');
-    mobile_search_input.focus();
-})
-
-mobile_search_input.addEventListener('keypress', (e)=>{
-    if(e.key == "Enter"){
-        search(mobile_search_input.value);
-        mobile_search_input.blur();
-        mobile_search_input.value ="";
-        mobile_search_input.classList.add('hidden');
-        mobile_search.classList.remove('hidden');
-    }
-})
-
-mobile_search_input.addEventListener('blur', () =>{
-    mobile_search_input.classList.add('hidden');
-    mobile_search.classList.remove('hidden');
-})
-
-// ----mobile search ----------//
-
-
-
 // ------- 封面 Img ------------//
 let campaigns;
 let content_array;
@@ -323,5 +163,180 @@ function removeCircle(){
         circleBlock.removeChild(circleBlock.firstChild);
     }
 }
+
+// -------------------------------------------------------//
+
+
+
+
+
+
+
+// ----paging scroll -----//
+function scroll(){
+    let container = document.querySelector('.category');
+    let distance  = container.getBoundingClientRect().bottom - window.innerHeight;
+    
+    if( distance < triggerDistance){
+
+        callAPI(createProduct);
+    }
+}
+
+window.addEventListener('scroll', scroll);
+
+// ------------------------------------------------------------//
+
+
+
+//-----change menu 
+function changeProductType(productType){
+        let e = event.target;
+       
+        //remove old product
+        removeProduct();
+
+        // active menu add color
+        let menuAnchar = document.querySelectorAll('.nav-left .menu a')
+        menuAnchar.forEach(a => a.classList.remove('isActive'));
+        e.classList.add('isActive');
+
+        //call api\
+        stylish.api = "products";
+        stylish.productType = productType;
+        src= "https://api.appworks-school.tw/api/1.0/" + stylish.api + "/" + stylish.productType;
+        callAPI(createProduct);       
+}
+
+
+//移除category
+function removeProduct(){
+    let category = document.querySelector('.category');
+    while(category.firstChild){
+        category.removeChild(category.firstChild);
+    }
+}
+
+
+//生成category
+function createProduct(res){
+    let  categoryBlock = document.querySelector('.category');
+    let  productLink, productBlock, newTag, productImg, colorDiv, colorBlock, productName, productPrice;
+    
+
+    res.data.forEach( (item ,index)=> {
+
+        productLink = document.createElement('a');
+        productLink.setAttribute('id', item.id);
+        productLink.setAttribute('href', `/product.html?id=${item.id}`);
+
+        // product block
+        productBlock = document.createElement('div');
+        productBlock.classList.add("category-block");
+        
+
+        // new tag
+        if(index === 0){
+            newTag = document.createElement('div');
+            newTag.classList.add("new");
+            newTag.textContent = "新品";
+            productBlock.appendChild(newTag);
+        }
+
+        //product Img
+        productImg = document.createElement('img');
+        productImg.classList.add("sample");
+        productImg.src = item.main_image;
+
+        // product name
+        productName = document.createElement('p');
+        productName.classList.add("sample-name");
+        productName.textContent = item.title;
+
+
+        // color
+        colorDiv = document.createElement('div');
+        item.colors.forEach( color => {
+            colorBlock = document.createElement('div');
+            colorBlock.classList.add("color");
+            colorBlock.style.backgroundColor = "#" + color.code;
+            if(color.code.toUpperCase() === 'FFFFFF'){
+                colorBlock.classList.add("color-border");
+            }
+            colorDiv.appendChild(colorBlock);
+        } )
+
+
+        // price
+        productPrice = document.createElement('p');
+        productPrice.classList.add("sample-price");
+        productPrice.textContent ='TWD.'+ item.price.toString();
+        
+        //append child
+        productBlock.appendChild(productImg);
+        productBlock.appendChild(colorDiv);
+        productBlock.appendChild(productName);
+        productBlock.appendChild(productPrice);
+
+        productLink.appendChild(productBlock);
+        categoryBlock.appendChild(productLink);
+    })
+}
+
+
+//------------search ----------------
+function search(value){
+    
+    stylish.productType = "search";
+    src= "https://api.appworks-school.tw/api/1.0/" + stylish.api + "/" + stylish.productType + "?keyword=" +value;
+ 
+    
+    removeProduct();
+    callAPI(createProduct);
+
+    let input = document.querySelector('#search');
+    input.value ="";
+}
+
+
+function err_page(){
+
+    removeProduct()
+
+    let  category = document.querySelector('.category');
+    let  productBlock = document.createElement('div');;
+    productBlock.textContent = "請重新搜尋";
+    category.appendChild(productBlock);
+}
+
+
+// ----mobile search ----------//
+let mobile_search = document.querySelector('.mobile-search-img');
+let mobile_search_input = document.querySelector('.mobile-search-input');
+
+mobile_search.addEventListener('click', () =>{
+    mobile_search.classList.add('hidden');
+    mobile_search_input.classList.remove('hidden');
+    mobile_search_input.focus();
+})
+
+mobile_search_input.addEventListener('keypress', (e)=>{
+    if(e.key == "Enter"){
+        search(mobile_search_input.value);
+        mobile_search_input.blur();
+        mobile_search_input.value ="";
+        mobile_search_input.classList.add('hidden');
+        mobile_search.classList.remove('hidden');
+    }
+})
+
+mobile_search_input.addEventListener('blur', () =>{
+    mobile_search_input.classList.add('hidden');
+    mobile_search.classList.remove('hidden');
+})
+
+// ----mobile search ----------//
+
+
 
 
---
+++ students/josh/stylish/public/product.js
@@ -0,0 +1,143 @@
+
+// let stylish = {
+//     api: "products",
+//     productType: "details" 
+// }
+
+// //------　api url
+// let src= "https://api.appworks-school.tw/api/1.0/" + stylish.api + "/" + stylish.productType;
+
+//------宣告 --------
+let isLoading = false;
+let datails;
+
+let details_main_image =  document.querySelector('#main-img');
+let details_product_name = document.querySelector('#product-name');
+let details_product_id = document.querySelector('#product-id');
+let details_product_price = document.querySelector('#product-price');
+let details_product_colors = document.querySelector('#product-colors');
+let details_product_sizes = document.querySelector('#product-sizes');
+let details_product_note = document.querySelector('#product-note');
+let details_product_story = document.querySelector('.story');
+let details_product_image = document.querySelectorAll('.product-image');
+let note = document.querySelector('#note');
+let texture = document.querySelector('#texture');
+let description = document.querySelector('#description');
+let wash = document.querySelector('#wash');
+let place = document.querySelector('#place');
+
+
+let productURL = new URL(location.href);
+const { href, protocol, hostname, pathname, search, searchParams } = productURL;
+
+
+// call detail api
+function callDetail(callback){
+
+    let src = "https://api.appworks-school.tw/api/1.0/products/details" + search; 
+
+    if ( !isLoading ) {
+        
+        isLoading = true;
+
+        fetch(src)
+        .then((res) => {
+            isLoading = false;
+            return res.json(); 
+         }).then((result) => {
+            details = result;
+            callback(result);
+
+        }).catch(function(err){
+        console.log("Fetch 錯誤:"+err);
+        });
+    }
+}
+ 
+callDetail(createDetails);
+
+function createDetails(res){
+
+    // main img
+    details_main_image.setAttribute('src', res.data.main_image);
+    // name
+    details_product_name.textContent = res.data.title; 
+    // id
+    details_product_id.textContent = res.data.id;
+    // price
+    details_product_price.textContent = 'TWD.' + res.data.price;
+    
+    //color 
+    res.data.colors.forEach( (item, index) => {
+        let color = document.createElement('div');
+        if( index === 0){
+            color.classList.add('current');
+        }
+        color.classList.add('color');
+        color.style.backgroundColor = "#" + item.code ;
+        details_product_colors.appendChild(color);
+    });
+
+
+    // size
+    res.data.sizes.forEach( (item, index) =>{
+        let size = document.createElement('div');
+        if( index === 0){
+            size.classList.add('current');
+        }
+        size.classList.add('size');
+        size.textContent = item;
+        details_product_sizes.appendChild(size);
+    })
+
+
+    // note
+    note.textContent = res.data.note;
+    texture.textContent = res.data.texture;
+    description.textContent = res.data.description;
+    description.style.whiteSpace = "pre";
+    wash.textContent = res.data.wash;
+    place.textContent = res.data.place;
+
+
+    //story
+    details_product_story.textContent = res.data.story;
+
+    //more img
+    details_product_image.forEach( (item, index) =>{
+        let more_img = document.createElement('img');
+        more_img.setAttribute('src', res.data.images[index]);
+        item.appendChild(more_img);
+    });
+}
+
+
+//------------search ----------------
+
+
+// ----mobile search ----------//
+let mobile_search = document.querySelector('.mobile-search-img');
+let mobile_search_input = document.querySelector('.mobile-search-input');
+
+mobile_search.addEventListener('click', () =>{
+    mobile_search.classList.add('hidden');
+    mobile_search_input.classList.remove('hidden');
+    mobile_search_input.focus();
+})
+
+mobile_search_input.addEventListener('keypress', (e)=>{
+    if(e.key == "Enter"){
+        search(mobile_search_input.value);
+        mobile_search_input.blur();
+        mobile_search_input.value ="";
+        mobile_search_input.classList.add('hidden');
+        mobile_search.classList.remove('hidden');
+    }
+})
+
+mobile_search_input.addEventListener('blur', () =>{
+    mobile_search_input.classList.add('hidden');
+    mobile_search.classList.remove('hidden');
+})
+
+// ----mobile search ----------//
